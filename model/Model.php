bb83f3dfd1ab88d1ad4388b56d2a58fe615cc38d
brahim <57845564+biko2020@users.noreply.github.com>
2020-09-23 22:45:41 +0100

    Add files via upload
    model

---
+++ model/Model.php
@@ -0,0 +1,50 @@
+<?php 
+
+abstract class Model {
+
+    // information sur la connexion et la base de donnees
+  
+
+    private $host = "localhost:3307";
+    private $db_name = "elctrostore";
+    private $username = "root";
+    private $password = "";
+    
+
+
+    // propriete contenant la connexion
+
+    protected $_connexion;
+   
+
+
+    // variable de requet table et id
+
+    public $table;
+    public $id;
+
+    
+    public function getConnexion() {
+      
+        // vider la propriete _connexion au debut de la connexion
+        $this->_connexion = null;
+
+        // affecter les parametres de connexion si tout passe bien
+        try {
+            $this->_connexion = new PDO('mysql:host=' .$this->host .';
+            dbname='. $this->db_name, $this->username, $this->password);  
+        
+        // executer la connexion avec l'unicode entre client et server
+            $this->_connexion->exec('set names utf8'); 
+          
+
+        // sinon afficher erreur            
+        } catch (PDOException $exception) {
+            echo 'Erreur :' .$exception->getMessage();
+        }
+
+    } 
+
+}
+
+?>
---
+++ model/Parametre.php
@@ -0,0 +1,167 @@
+<?php
+
+class Parametre extends Model {
+
+ // etendre notre model sur la classe Parametre
+  
+    public function __construct() { 
+        // recuperer la fonction getconnexion
+        $this->getConnexion();     
+    
+    }
+
+
+    public function getTableName($table) {
+        $this->table = $table;
+    }
+
+    /**
+     * @param nom  nom de l'utilisateur
+     * @param prenom
+     * @param email
+     * @param gsm
+     * @param dte
+     * @param heure
+     * @return array tableau contenant les infos des users
+     * @return TRUE
+     * @return FALSE 
+     * @return NULL
+     */
+
+    public function createPatient($nom,$prenom,$email,$gsm,$dte,$heure) {
+      
+        $sql = "INSERT INTO $this->table (nom,prenom,email,gsm,dte,heure) VALUES (:nom,:prenom,:email,:gsm,:dte,:heure)";
+
+        try {
+            $result = $this->_connexion->prepare($sql);
+            
+            $var = $result->execute(array(
+                ':nom'    => $nom,
+                ':prenom' => $prenom,
+                ':email'  => $email,
+                ':gsm'    => $gsm,
+                ':dte'    => $dte,
+                ':heure'  => $heure
+            ));
+
+            if($var){
+               return TRUE;
+            } else {
+                return FALSE;                                  
+            }
+            
+        } catch (PDOException $th) {
+            
+                return NULL;
+               
+        }
+        
+
+    }
+
+    # selection des file d'attente 
+ 
+    public function getFileAttente() {
+
+        $sql = "SELECT * FROM $this->table";
+
+        try {
+
+            $result = $this->_connexion->prepare($sql);
+            $var = $result->execute();
+            $data = $result->fetchAll(PDO::FETCH_ASSOC);
+
+                if($var) {
+                    return $data;
+                } else {
+                    return FALSE;
+                }
+
+        } catch (PDOException $th) {
+
+            return NULL;
+        }   
+
+    }
+
+    # Creer des sessions pour les medecins
+       /**
+     * @param nom
+     * @param email
+     * @param password
+     * @param True cas de succes 
+     * @return Null dans le cas d'exception
+     */
+    public function CreatUsers($nom,$email,$password) {
+
+        $sql ="INSERT INTO $this->table(nom,email,password) VALUES (:nom,:email,:password)";
+        
+        try {
+
+            $result = $this->_connexion->prepare($sql);
+            $var = $result->execute(array(
+
+                ':nom'     => $nom,
+                ':email'   => $email,
+                ':password'=> sha1($password)
+
+            ));
+
+            if($var) {
+                return TRUE;
+            } else {
+                return FALSE;
+            }
+
+        } catch (PDOException $th) {
+            return NULL;
+        }
+
+    }
+    
+    # fonction qui verifier la session d'un user
+    /**
+     * @param email
+     * @param password
+     * @return tableau return les data saisi par user
+     * @return False cas d'identification éhouée
+     * @return Null cas d'exception 
+     */
+
+    public function session($email, $password) {
+       
+        $sql = "SELECT * FROM $this->table WHERE email = :email";
+        
+        try {
+           
+            
+            $result = $this->_connexion->prepare($sql);
+            $result->execute(array(
+                ':email' => $email
+            ));
+           
+            $data = $result->fetch(PDO::FETCH_ASSOC);
+        
+                if($data && ($data['password'] == sha1($password))) { //teste si le mot de passe est correct et crypté ce dernier avec la fonction sha1
+                   
+                    unset($data['password']);//unset permete de masquer le mot de passe 
+                    return $data;
+
+                } else {
+
+                    return False;
+                }
+
+        } catch (PDOException $th) {
+            return NULL;
+        }
+
+    }
+
+
+} # ------fermeture class
+
+
+?>
+
+
