a4271b3c49904cf4393aa1c5533749dad96260c4
brahim <57845564+biko2020@users.noreply.github.com>
2020-09-26 18:42:37 +0100

    Add files via upload
    add a new items in our menu index

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@ $url = explode('/',$url);
 
 
 // notre route
-$route = array("Accueil", "Rendezvous", "Fileattente", "InscriptionRendezVous","Administration", "Inscription", "Actionconnexion", "Actiondeconnexion", "GestionFileAttente","Contact");
+$route = array("Accueil", "Rendezvous", "Fileattente", "InscriptionRendezVous","Administration", "Inscription", "Actionconnexion", "Actiondeconnexion", "GestionFileAttente", "modifier", "supprimer", "updatepatient", "Contact");
 
 
 $action = $url[0]; // notre action recupere le premier element de notre tableau
